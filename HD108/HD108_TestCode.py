97906f0b57f57c4fd5aca38f4e2d75902b45735e
Yumeng Zhuang <calthayzh@gmail.com>
2025-10-01 17:13:19 -0700

    animated HD108 with rpi but cannot turn off

--- HD108/HD108_TestCode.py
+++ HD108/HD108_TestCode.py
@@ -4,10 +4,10 @@ import time
 def send_hd108_colors(colors_16bit, global_brightness=15):
     spi = spidev.SpiDev()
     spi.open(0, 0)                                            # Open SPI bus 0, device 0
-    spi.max_speed_hz = 10000000                               # Set speed to 10MHz: 32000000 maximum
+    spi.max_speed_hz = 2000000                               #2M # Set speed to 10MHz: 32000000 maximum
     
     data = []
-    data.extend([0x00] * 16)  # Start frame: HD108 protocol requires 128-bit start frame before LED data
+    data.extend([0x00] * 8)  #16 # Start frame: HD108 protocol requires 128-bit of zeros start frame before LED data
     
     for r16, g16, b16 in colors_16bit:
         # 5-bit brightness (0-31)
@@ -16,22 +16,30 @@ def send_hd108_colors(colors_16bit, global_brightness=15):
         # 16-bit brightness field: 1 + 5 bits brightness + 10 bits padding
         byte1 = 0x80 | (brightness_5bit << 2)
         byte2 = 0x00
+        # as (1)(5bit)(5bit)(5bit) brightnesses
+        
         
         data.extend([
-            byte1, byte2,                              # Brightness
-            (b16 >> 8) & 0xFF, b16 & 0xFF,             # Blue 16-bit
+            0xFF, 0xFF,                              # Brightness max first
+            (r16 >> 8) & 0xFF, r16 & 0xFF,             # Blue 16-bit
             (g16 >> 8) & 0xFF, g16 & 0xFF,             # Green 16-bit
-            (r16 >> 8) & 0xFF, r16 & 0xFF              # Red 16-bit
+            (b16 >> 8) & 0xFF, b16 & 0xFF              # Red 16-bit
         ])
     
-    data.extend([0xFF] * 16)  # End frame
-    spi.writebytes2(data)
+    # data.extend([0xFF] * 8)  # End frame #? 
+    num_end = 2 * (len(colors_16bit) + 1) 
+    print(num_end) 
+    data.extend([0xFF] * num_end)
+    spi.writebytes(data)
     spi.close()
 
 # Test code
 test_colors = [
     (65535, 0, 0), (0, 65535, 0), (0, 0, 65535),
-    (65535, 65535, 0), (32767, 32767, 32767)
+    (65535, 65535, 0), (32767, 32767, 32767),
+    (65535, 0, 0), (0, 65535, 0), (0, 0, 65535),
+    (65535, 65535, 0), (32767, 32767, 32767),
+    (65535, 0, 0)
 ]
 send_hd108_colors(test_colors, global_brightness=31)
 time.sleep(2)
--- HD108/animation_hd108.py
+++ HD108/animation_hd108.py
@@ -3,22 +3,12 @@ import numpy as np
 import spidev
 import time
 
-start_color_rgb = (164, 0, 214)
-end_color_rgb = (255, 255, 0)
+start_color_rgb = (164/255, 0, 214/255)
+end_color_rgb = (255/255, 255/255, 0)
 
 frames = []
 total_steps = 255
 num_pixels = 11
-
-# Convert sRGB to Oklab
-srgb_color = [0.5, 0.2, 0.8]  # Example sRGB color
-oklab_color = colour.convert(srgb_color, "sRGB", "Oklab")
-print(oklab_color)
-
-# Convert Oklab to sRGB
-oklab_color_back = [0.4, 0.1, -0.3] # Example Oklab color
-srgb_color_back = colour.convert(oklab_color_back, "Oklab", "sRGB")
-print(srgb_color_back)
 
 start_color_oklab = colour.convert(start_color_rgb, "sRGB", "Oklab")
 end_color_oklab = colour.convert(end_color_rgb, "sRGB", "Oklab")
@@ -28,28 +18,40 @@ end_color = np.array(end_color_oklab)
 
 
 for step in range(total_steps):
-    start_color_step = (end_color - start_color)/total_steps * step + start_color
-    end_color_step = end_color - (end_color - start_color)/total_steps * step
+    # start_color_step = (end_color - start_color)/total_steps * step + start_color
+    # end_color_step = end_color - (end_color - start_color)/total_steps * step
     frame = []
+    position = step/total_steps*num_pixels
     for pixel in range(num_pixels):
-        color_step = (end_color_step - start_color_step)/num_pixels * pixel + start_color_step
+        # color_step = (end_color_step - start_color_step)/num_pixels * pixel + start_color_step
+        if pixel < position:
+            color_step = (end_color - start_color)/position * pixel + start_color
+        else:
+            color_step = (start_color - end_color)/(num_pixels - position) * (pixel - position) + end_color
+            
         srgb_color_step = colour.convert(color_step, "Oklab", "sRGB")
-        srgb = int(np.array(srgb_color_step)*2**16).astype(np.int16)
-        frame.append(srgb)
+        srgb = (np.array(srgb_color_step) * (2**16 - 1)).astype(np.uint16)
+        frame.append(tuple(srgb))
+    # print(srgb)
     frames.append(frame)
 
 
 def send_hd108_colors(colors_16bit, global_brightness=15):
     spi = spidev.SpiDev()
     spi.open(0, 0)                                            # Open SPI bus 0, device 0
-    spi.max_speed_hz = 20000000                               # Set speed to 10MHz: 32000000 maximum
+    spi.max_speed_hz = 2000000                               # Set speed to 10MHz: 32000000 maximum
     
     data = []
     data.extend([0x00] * 8)  # Start frame: HD108 protocol requires 64-bit start frame before LED data
     
-    for r16, g16, b16 in colors_16bit:
-
-        brightness = (1 << 15) | (global_brightness << 10) | (global_brightness << 5) | global_brightness
+    for r16, g16, b16 in list(colors_16bit):
+        r16, g16, b16 = map(int, (r16, g16, b16))
+        
+        # print(int(r16/255), int(g16/255), int(b16/255))
+        if global_brightness <= 0:
+            brightness = 0 #gives werid colors
+        else:
+            brightness = (1 << 15) | (global_brightness << 10) | (global_brightness << 5) | global_brightness
         
         data.extend([
             (brightness >> 8) & 0xFF, brightness & 0xFF,             # Brightness
@@ -57,15 +59,29 @@ def send_hd108_colors(colors_16bit, global_brightness=15):
             (g16 >> 8) & 0xFF, g16 & 0xFF,             # Green 16-bit
             (b16 >> 8) & 0xFF, b16 & 0xFF              # Red 16-bit
         ])
-    
-    data.extend([0xFF] * len(colors_16bit))  # End frame
-    spi.writebytes2(data)
+        
+    num_end = 2 * (len(colors_16bit) + 1) 
+    data.extend([0xFF] * num_end)  # End frame
+    spi.writebytes(data)
     spi.close()
 
-total_time = 15 #seconds
+total_time = 3 #seconds
 time_sleep = total_time/total_steps
 
 for frame in frames:
-    send_hd108_colors(frame, 15)
+    # print(frame)
+    send_hd108_colors(frame, global_brightness = 5)
+    #exit()
     time.sleep(time_sleep)
 
+for frame in frames:
+    # print(frame)
+    send_hd108_colors(frame, global_brightness = 5)
+    #exit()
+    time.sleep(time_sleep)
+    
+for frame in frames:
+    # print(frame)
+    send_hd108_colors(frame, global_brightness = 5)
+    #exit()
+    time.sleep(time_sleep)
